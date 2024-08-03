cbc8d891ba9c442331a220c83190b29bf96d6039
parkerj44 <johnson.7725@osu.edu>
2024-08-03 17:12:05 -0400

    Use requests module to grab HN data

--- scrape.py
+++ scrape.py
@@ -1,6 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 
 response = requests.get('https://news.ycombinator.com/news')
-print(response.text)
+html_object = BeautifulSoup(response.text, 'html.parser')
+links = html_object.select('.storylink')
+subtext = html_object.select('.subtext')
+
+
+def sorted_stories(list):
+    return sorted(list)
+
+# Creates personal Hacker News if points > 100
+def personal_hn(links, subtext):
+    hacker_news = []
+
+    for index, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[index].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace('points', ''))
+            if points > 99:
+                hacker_news.append({'title': title, 'link': href, 'votes': points})
+    return sorted_stories(hacker_news)
+
+
+pprint.pprint(personal_hn(links, subtext))
